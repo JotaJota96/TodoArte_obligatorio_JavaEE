e3a6b8758566e5ab327890a46da88b13efd6b43a
JotaJota96 <jjap96@gmail.com>
2020-05-06 21:48:53 -0300

    Firmas Juan

--- TodoArteEJB/ejbModule/com/TodoArte/FachadeControllers/BackOfficeController.java
+++ TodoArteEJB/ejbModule/com/TodoArte/FachadeControllers/BackOfficeController.java
@@ -1,7 +1,69 @@
 package com.TodoArte.FachadeControllers;
 
+import java.util.ArrayList;
+
+import com.TodoArte.Classes.Administrador;
+import com.TodoArte.Classes.CategoriaContenido;
+import com.TodoArte.Classes.CategoriaSitio;
+import com.TodoArte.Classes.NotificacionArtista;
+import com.TodoArte.Classes.PagoAPlataforma;
+import com.TodoArte.Classes.Reporte;
 import com.TodoArte.FachadeInterfaces.BackOfficeInterface;
 
 public class BackOfficeController implements BackOfficeInterface {
+
+	@Override
+	public void bloquearDesbloquearArtista(String idArtista) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bloquearDesbloquearContenido(int idContenido) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Reporte> obtenerReportes(int idContenido) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void notificarArtista(String idArtista, NotificacionArtista notificacion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void agregarCategoriaDeSitio(CategoriaSitio categoriaSitio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void agregarCategoriaDeContenido(CategoriaContenido categoriaContenido) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean iniciarSesion(String id, String contrasenia) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<PagoAPlataforma> obtenerPagos(String idArtista) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void agregarAdministrador(Administrador administrador) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- TodoArteEJB/ejbModule/com/TodoArte/FachadeInterfaces/FrontOfficeInterface.java
+++ TodoArteEJB/ejbModule/com/TodoArte/FachadeInterfaces/FrontOfficeInterface.java
@@ -1,5 +1,117 @@
 package com.TodoArte.FachadeInterfaces;
 
-public interface FrontOfficeInterface {
+import java.util.ArrayList;
 
+import javax.ejb.Local;
+
+import com.TodoArte.Classes.Artista;
+import com.TodoArte.Classes.Comentario;
+import com.TodoArte.Classes.Contenido;
+import com.TodoArte.Classes.Fan;
+import com.TodoArte.Classes.QyAProgramado;
+import com.TodoArte.Classes.Sitio;
+import com.TodoArte.Classes.Usuarios;
+
+@Local
+public interface FrontOfficeInterface {
+	
+	/**
+	 * Registra un nuevo fan en el sistema
+	 * @param Fan Fan a registrar
+	 */
+	public void registrarUsuarioFan(Fan fan);
+	
+	/**
+	 * Registra un nuevo artista y su sitio en el sistema
+	 * @param artista Artista a registrar
+	 * @param sitio Sitio del artista a registrar
+	 */
+	public void registrarUsuarioArtista(Artista artista, Sitio sitio);
+
+	/**
+	 * Valida los datos de inicio de sesion de un usuario (sea fan o artista)
+	 * Si el usuario se encuentra bloqueado, no se le permite iniciar sesion y se retorna FALSE
+	 * @param idUsuario ID (nickname o email) del usuario que desea iniciar sesion
+	 * @param contrasenia Contrasenia del usuario
+	 * @return TRUE si los datos son correctos, de lo contrario FALSE
+	 */
+	public boolean iniciarSesion(String idUsuario, String contrasenia);
+	
+	/**
+	 * Agrega en el sistema, un contenido a un artista. Si el artista ya tiene un contenido con el ID recibido, lo actualiza
+	 * @param idArtista ID del artista propietario del contenido
+	 * @param contenido Contenido a agregar o modificar
+	 */
+	public void agregarModificarContenido(String idArtista, Contenido contenido);
+	
+	/**
+	 * Elimina un contenido especifico de un artista especifico
+	 * @param idArtista ID del artista propietario del contenido
+	 * @param idContenido ID del contenido a eliminar
+	 */
+	public void eliminarContenido(String idArtista, int idContenido);
+	
+	/**
+	 * Programa un QyA para un artista
+	 * @param idArtista ID del artista que realizará el QyA
+	 * @param qyaProgramado QyA a programar
+	 */
+	public void programarQyA(String idArtista, QyAProgramado qyaProgramado);
+	
+	/**
+	 * Devuelve los datos del usuario, sea Fan o Artista
+	 * @param idUsuario ID del usuario que se desea obtener
+	 * @return Si el usuario existe, devuelve sus datos, de lo contrario NULL
+	 */
+	public Usuarios obtenerDatosUsuario(String idUsuario);
+	
+	/**
+	 * Recarga el saldo de un usuario, sea fan o artista
+	 * @param idUsuario ID del usuario al que se le quiere recargar su saldo
+	 * @param monto Monto que se desea agregar al saldo actual
+	 */
+	public void recargarSaldo(String idUsuario, float monto);
+	
+	/**
+	 * Obtiene un contenido especifico de un artista especifico
+	 * @param idArtista ID del artista propietario del contenido que se desea obtener
+	 * @param idContenido ID del contenido que se desea obtener
+	 * @param idFan ID del fan que quiere obtener la informacion (idFan = "" si el usuario es invitado)
+	 * @return Si el artista posee un contenido con ese ID, lo devuelve, de lo contrario retorna NULL
+	 */
+	public Contenido obtenerContenido(String idArtista, int idContenido, String idFan);
+	
+	/**
+	 * Devuelve todo los contenidos de un artista especifico
+	 * @param idArtista ID del artista propietario del contenido que se desea obtener
+	 * @param idFan ID del fan que quiere obtener la informacion (idFan = "" si el usuario es invitado)
+	 * @return Si el ID es correcto, devuelve los contenidos del artista, de lo contrario retorna NULL
+	 */
+	public ArrayList<Contenido> obtenerContenido(String idArtista, String idFan);
+	
+	/**
+	 * Bloquea o desbloque a un fan del sitio de un artista. Si el fan no se encuentra bloqueado aun, lo bloquea, si ya se encuentra bloqueado, lo desbloquea.
+	 * @param idArtista ID del artista del sitio afectado
+	 * @param idFan ID del fan a bloquear del sitio
+	 */
+	public void bloquearDesbloquearUsuarioDeSitio(String idArtista, String idFan);
+	
+	/**
+	 * Devuelve todos los fans que siguen al sitio del artista especificado
+	 * @param idArtista ID del artista del que se desea conocer sus fans seguidores
+	 * @return Si el ID es correcto, devuelve los fans del sitio, de lo contrario retorna NULL
+	 */
+	public ArrayList<Fan> obtenerFansDeSitio(String idArtista);
+	
 }
+
+
+
+
+
+
+
+
+
+
+
