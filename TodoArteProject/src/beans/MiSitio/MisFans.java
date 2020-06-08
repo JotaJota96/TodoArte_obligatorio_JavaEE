3f9f62e8745d1440eed523c416bb250b8a287673
alfa jor 144 <karloxx09@gmail.com>
2020-06-08 18:30:16 -0300

    casi hecho misFans

--- TodoArteProject/src/beans/MiSitio/MisFans.java
+++ TodoArteProject/src/beans/MiSitio/MisFans.java
@@ -1,8 +1,8 @@
 package beans.MiSitio;
 
 import java.io.Serializable;
+import java.sql.Date;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -10,16 +10,17 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Named;
 
-import com.TodoArte.Classes.Artista;
 import com.TodoArte.Classes.Fan;
+import com.TodoArte.Classes.PagoAPlataforma;
 import com.TodoArte.Classes.Usuario;
+import com.TodoArte.Enums.Sexo;
 import com.TodoArte.FachadeControllers.BackOfficeController;
 import com.TodoArte.FachadeControllers.FrontOfficeController;
 import com.TodoArte.FachadeInterfaces.BackOfficeInterface;
 import com.TodoArte.FachadeInterfaces.FrontOfficeInterface;
 
 @Named
-@RequestScoped
+@SessionScoped
 public class MisFans implements Serializable {
 	
 	//----------atrivutos------------------------------
@@ -32,8 +33,31 @@ public class MisFans implements Serializable {
 	
 	//-----------funciones-------------------------------
 	
+	public void bloquearDesbloquearFan(String idFan) {
+		String idArtista = "ergo"; //idArtista obtenido por la variable secion
+		
+		fo.bloquearDesbloquearUsuarioDeSitio(idArtista, idFan);
+	}
 	
+	public String estaBloqueadoValue(String nikname) {
+		Usuario user = fo.obtenerDatosUsuario(nikname);
+		if(user.getBloqueado()) {
+			return "Desbloquear";
+		}
+		else {
+			return "Bloquear";
+		}
+	}
 	
+	public String estaBloqueadoClass(String nikname) {
+		Usuario user = fo.obtenerDatosUsuario(nikname);
+		if(user.getBloqueado()) {
+			return "btn  btn-success";
+		}
+		else {
+			return "btn  btn-danger";
+		}	
+	}
 	
 	//----------setters getters constructors---------
 	public ArrayList<Fan> getListaFan() {
@@ -45,6 +69,18 @@ public class MisFans implements Serializable {
 	}
 
 	public MisFans() {
+	}
+	
+	@PostConstruct
+	public void init() {
+		
+		String idArtista = "ergo"; //idArtista obtenido por la variable secion
+		
+		//fo.suscribirseFanArtista("alfajor", "ergo"); //esto altero la base de datos
+				
+		listaFan = fo.obtenerFansDeSitio(idArtista);
+		
+		System.out.println("---------------------listaFan--------------" + listaFan.size());
 	}
 	
 }
