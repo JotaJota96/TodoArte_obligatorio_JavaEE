736b6bdd752ccaed5be7fe1e6c0754e62c12ca32
JotaJota96 <jjap96@gmail.com>
2020-06-19 22:54:24 -0300

    Correccion de tildes en datos de prueba

--- TodoArteProject/src/com/TodoArte/Pruebas.java
+++ TodoArteProject/src/com/TodoArte/Pruebas.java
@@ -8,8 +8,6 @@ import com.TodoArte.Enums.*;
 import com.TodoArte.FachadeControllers.BackOfficeController;
 import com.TodoArte.FachadeControllers.FrontOfficeController;
 
-import beans.FuncionesComunes;
-
 public class Pruebas {
 
 	public static void main(String[] args) {
@@ -29,8 +27,8 @@ public class Pruebas {
 		if (log) System.out.println("Creado Administrador: admin, 1234");
 		
 		CategoriaSitio categoriasSitio[] = {
-				new CategoriaSitio(0, "M�sica"),
-				new CategoriaSitio(0, "Fotograf�a"),
+				new CategoriaSitio(0, "Música"),
+				new CategoriaSitio(0, "Fotografía"),
 				new CategoriaSitio(0, "Pintura"),
 				new CategoriaSitio(0, "Videos"),
 		};
@@ -40,8 +38,8 @@ public class Pruebas {
 		}
 		// ----------------------------------------------------------
 		CategoriaContenido categoriasContenido[] = {
-				new CategoriaContenido(0, "M�sica"),
-				new CategoriaContenido(0, "Fotograf�a paisajistica"),
+				new CategoriaContenido(0, "Música"),
+				new CategoriaContenido(0, "Fotografía paisajistica"),
 				new CategoriaContenido(0, "Comics"),
 				new CategoriaContenido(0, "Pintura"),
 				new CategoriaContenido(0, "Videoclip musical"),
@@ -80,7 +78,7 @@ public class Pruebas {
 								categoriasSitio[0], 
 								null, 
 								fuentes[0])),
-				new Artista("ArdeTroya", "1234", "arde@troya.com", 
+				new Artista("ardetroya", "1234", "arde@troya.com", 
 						0, null, 
 						"Arde Troya", "Una banda de covers del peyote y cosas por el estilo", 
 						new Sitio(0, null, 0, 
@@ -95,8 +93,8 @@ public class Pruebas {
 								fuentes[1])),
 				new Artista("picasso", "1234", "pablo@picasso.com", 
 						0, null, 
-						"Pablo Picasso", "Descripci�n\n" + 
-								"Pablo Ruiz Picasso fue un pintor y escultor espa�ol, creador, junto con Georges Braque, del cubismo.", 
+						"Pablo Picasso", "Descripción\n" + 
+								"Pablo Ruiz Picasso fue un pintor y escultor español, creador, junto con Georges Braque, del cubismo.", 
 						new Sitio(0, null, 0, 
 								"540026", "82013c", "fcebf3", 
 								"https://www.youtube.com/channel/UCDupeqPlIEnjmtPmUEvgvLg", 
@@ -109,8 +107,8 @@ public class Pruebas {
 								fuentes[2]))
 		};
 		NotificacionArtista notificacionesArtista[] = {
-				new NotificacionArtista(0, "�Bienvenido!", "�Bienvenido a la plataforma TodoArte!", new Date(2020,00,01)),
-				new NotificacionArtista(0, "�Sube contenido!", "No esperes mas, comienza a subir contenido a tu sitio", new Date(2020,00,01))
+				new NotificacionArtista(0, "!Bienvenido!", "!Bienvenido a la plataforma TodoArte!", new Date(2020,00,01)),
+				new NotificacionArtista(0, "!Sube contenido!", "No esperes mas, comienza a subir contenido a tu sitio", new Date(2020,00,01))
 		};
 		for (int i = 0; i < artistas.length; i++) {
 			artistas[i] = fo.registrarUsuarioArtista(artistas[i], artistas[i].getMiSitio());
@@ -126,19 +124,19 @@ public class Pruebas {
 						0, null, 
 						"Juan", "Alvarez", 
 						new Date(1996,4,11), 
-						"San Jos� de Mayo", 
+						"San José de Mayo", 
 						Sexo.Masculino),
 				new Fan("luc31g", "1234", "luc31g@gmail.com", 
 						0, null, 
 						"Lucas", "Garrido", 
 						new Date(1998,11,31), 
-						"San Jos� de Mayo", 
+						"San Jos de Mayo", 
 						Sexo.Masculino),
 				new Fan("alfajor", "1234", "alfajor@gmail.com", 
 						0, null, 
 						"Carlos", "Balbiani", 
 						new Date(1992,6,16), 
-						"San Jos� de Mayo", 
+						"San José de Mayo", 
 						Sexo.Neutro)
 		};
 		for (int i = 0; i < fans.length; i++) {
